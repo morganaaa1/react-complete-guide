041e51e0271bb7c9c3f8d61d6d0923fc8dd949bd
ali <alinabilahramadhan@gmail.com>
2023-01-18 11:08:21 +0700

    splitting Components Into Multiple Components

--- src/App.js
+++ src/App.js
@@ -35,23 +35,23 @@ function App() {
       <ExpenseItem
         title={expense[0].title}
         amount={expense[0].amount}
-        date={expense[0].date}>
-      </ExpenseItem>
+        date={expense[0].date}
+        />
       <ExpenseItem
         title={expense[1].title}
         amount={expense[1].amount}
-        date={expense[1].date}>
-      </ExpenseItem>
+        date={expense[1].date}
+        />
       <ExpenseItem
         title={expense[2].title}
         amount={expense[2].amount}
-        date={expense[2].date}>
-      </ExpenseItem>
+        date={expense[2].date}
+        />
       <ExpenseItem
         title={expense[3].title}
         amount={expense[3].amount}
-        date={expense[3].date}>
-      </ExpenseItem>
+        date={expense[3].date}
+        />
     </div>
   );
 }
