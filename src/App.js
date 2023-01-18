c2216fc721592274c6c1244ee508c37100e40606
ali <alinabilahramadhan@gmail.com>
2023-01-18 13:10:40 +0700

    Practice React and Components Basics

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
-import ExpenseItem from "./components/ExpenseItem";
+import Expenses from "./components/Expenses";
 
-function App() {
+
+function App(props) {
 
   const expense = [
     {
@@ -32,26 +33,7 @@ function App() {
       <h1>
       Let's Get Started
       </h1>
-      <ExpenseItem
-        title={expense[0].title}
-        amount={expense[0].amount}
-        date={expense[0].date}
-        />
-      <ExpenseItem
-        title={expense[1].title}
-        amount={expense[1].amount}
-        date={expense[1].date}
-        />
-      <ExpenseItem
-        title={expense[2].title}
-        amount={expense[2].amount}
-        date={expense[2].date}
-        />
-      <ExpenseItem
-        title={expense[3].title}
-        amount={expense[3].amount}
-        date={expense[3].date}
-        />
+      <Expenses items={expense}/>
     </div>
   );
 }
