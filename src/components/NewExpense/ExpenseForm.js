53755ff9bf24e4dc964b3ea4b166a2194486d673
ali <alinabilahramadhan@gmail.com>
2023-01-18 16:05:10 +0700

    Listening to User Input

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -2,37 +2,42 @@ import React from 'react'
 import './ExpenseForm.css'
 
 function ExpenseForm() {
-  return (
-    <div>
-        <form>
-            <div className='new-expense__controls'>
-                <div className='new-expense__control'>
-                    <label>
-                        Title
-                    </label>
-                    <input type='text'/>
+
+    const titleChangeHandler = (event) => {
+        console.log(event.target.value);
+    }
+
+    return (
+        <div>
+            <form>
+                <div className='new-expense__controls'>
+                    <div className='new-expense__control'>
+                        <label>
+                            Title
+                        </label>
+                        <input type='text' onChange={titleChangeHandler}/>
+                    </div>
+                    <div className='new-expense__control'>
+                        <label>
+                            Amount
+                        </label>
+                        <input type='text' min='0.01' step='0.01'/>
+                    </div>
+                    <div className='new-expense__control'>
+                        <label>
+                            Date
+                        </label>
+                        <input type='date' min='2020-01-01' max='2023-12-31'/>
+                    </div>
                 </div>
-                <div className='new-expense__control'>
-                    <label>
-                        Amount
-                    </label>
-                    <input type='text' min='0.01' step='0.01'/>
+                <div className='new-expense__actions'>
+                    <button type='submit'>
+                        Add Expense
+                    </button>
                 </div>
-                <div className='new-expense__control'>
-                    <label>
-                        Date
-                    </label>
-                    <input type='date' min='2020-01-01' max='2023-12-31'/>
-                </div>
-            </div>
-            <div className='new-expense__actions'>
-                <button type='submit'>
-                    Add Expense
-                </button>
-            </div>
-        </form>
-    </div>
-  )
-}
+            </form>
+        </div>
+    )
+    }
 
 export default ExpenseForm
