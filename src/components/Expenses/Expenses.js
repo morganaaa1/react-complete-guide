ca4f99bc2d040c76ebdc5942b7707b766f5e125c
ali <alinabilahramadhan@gmail.com>
2023-01-19 14:48:18 +0700

    Adding Keys

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -17,6 +17,7 @@ function Expenses(props) {
             <ExpenseFilter selected={filterYear} onChangeFilter={filterChangeHandler}/>
             {
               props.items.map(expense => <ExpenseItem 
+                key={expense.id}
                 title={expense.title}
                 amount={expense.amount}
                 date={expense.date}
