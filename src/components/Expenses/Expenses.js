71f0d8b75da618ab60f9c24fbab64bfebf5aada3
ali <alinabilahramadhan@gmail.com>
2023-01-19 13:37:18 +0700

    try to use event and state

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,11 +1,20 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './Expenses.css'
 import ExpenseItem from './ExpenseItem'
 import Card from '../UI/Card'
+import ExpenseFilter from './ExpenseFilter'
 
 function Expenses(props) {
+
+  const [filterYear, setFilterYear] = useState('2021');
+
+  const filterChangeHandler = selectedYear => {
+    setFilterYear(selectedYear);
+  }
+
   return (
     <Card className='expenses'>
+            <ExpenseFilter selected={filterYear} onChangeFilter={filterChangeHandler}/>
             <ExpenseItem
                 title={props.items[0].title}
                 amount={props.items[0].amount}
